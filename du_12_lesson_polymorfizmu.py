4a8612fa4b969903a2563f56d344ce39b38b0b6e
anita <anita.bacova@gmail.com>
2026-01-19 17:32:41 +0100

    polymorfizmus a dedenie

---
+++ du_12_lesson_polymorfizmu.py
@@ -0,0 +1,22 @@
+# du_13_01_2026 polymorfizmu
+
+class Tvar:
+    def obsah(self):
+        print("vypocet obsahu")
+
+class Obdlznik(Tvar):
+    def obsah(self):
+        print("obdlznik")
+
+class Kruh(Tvar):
+    def obsah(self):
+        print("kruh")
+
+class Pravouhly_trojuholnik(Tvar):
+    def obsah(self):
+        print("pravouhly trojuholnik")
+
+tvary = [Obdlznik(), Kruh(), Pravouhly_trojuholnik()]
+
+for i in range(3):
+    tvary[i].obsah()

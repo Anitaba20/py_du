93e04cbdacb5c7aa183ba4be6d48accb1c0ca867
anita <anita.bacova@gmail.com>
2026-01-20 01:04:29 +0100

    dľžka úsečky

--- du_11_lesson_pocet roznych prvkov.py
+++ du_11_lesson_pocet roznych prvkov.py
@@ -16,3 +16,18 @@ for cislo in cisla:
         posledne_cislo = cislo
 
 print(pocet_cisel)
+
+
+# zadanie2_snakify_Dĺžka úsečky
+
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+
+print(distance(a, b, c, d))

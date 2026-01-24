dc9df1dd7212cc2b3882f40e153038e50092c167
Anita <anita.bacova@gmail.com>
2026-01-24 22:03:01 +0100

    lambda

---
+++ du_07_lesson_lambda.py
@@ -0,0 +1,16 @@
+# DU 7 transformuj_zoznam
+
+def transformuj_zoznam(zoznam, funkcia):
+    vysledok = []
+
+cisla1 = [23,42,10,5,11]
+for cislo in cisla1:
+    print((lambda x:x*2)(cislo))
+
+cisla2 = [23,42,10,5,11]
+for cislo in cisla2:
+    print((lambda x:x**2)(cislo))
+
+cisla3 = [23,42,10,5,11]
+for cislo in cisla3:
+    print((lambda x:x-2)(cislo))

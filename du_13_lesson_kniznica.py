24dd77392702fb548c6933f7a389e489eca39c1a
Anita <anita.bacova@gmail.com>
2026-01-26 19:34:26 +0100

    vytvorenie jednoduchého systému pre správu knižničných zdrojov

--- du_13_lesson_kniznica.py
+++ du_13_lesson_kniznica.py
@@ -65,3 +65,45 @@ kniznica_.pridaj_knihu(kniha2)
 print("Dostupné knihy:")
 kniznica_.zobraz_dostupne_knihy()
 
+while True:
+    print("\nNavigácia podľa čísla:")
+    print("1 Pridať knihu")
+    print("2 Vyhľadať knihu podľa názvu")
+    print("3 Vypožičať knihu podľa ISBN")
+    print("4 Vrátiť knihu podľa ISBN")
+    print("5 Zobraziť dostupné knihy")
+    print("6 Ukončiť program")
+
+    menu = input("Vyber číslo: ")
+
+    if menu == "1":
+        nazov = input("Zadaj názov knihy: ")
+        autor = input("Zadaj autora: ")
+        isbn = input("Zadaj ISBN: ")
+        rok = int(input("Zadaj rok vydania: "))
+        nova_kniha = Kniha(nazov, autor, isbn, True, rok)
+        kniznica_.pridaj_knihu(nova_kniha)
+
+    elif menu == "2":
+        nazov = input("Zadaj názov knihy: ")
+        kniznica_.vyhladaj_podla_nazvu(nazov)
+
+    elif menu == "3":
+        isbn = input("Zadaj ISBN knihy: ")
+        kniznica_.vypozicat_knihu(isbn)
+
+    elif menu == "4":
+        isbn = input("Zadaj ISBN knihy: ")
+        kniznica_.vratit_knihu(isbn)
+
+    elif menu == "5":
+        print("Dostupné knihy:")
+        kniznica_.zobraz_dostupne_knihy()
+
+    elif menu == "6":
+        print("Koniec programu.")
+        break
+
+    else:
+        print("Neplatná voľba, skús znova.")
+
